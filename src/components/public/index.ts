7dea58f130bed43e66bb10b3b8e230cf1d5e1858
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-04-21 11:54:23 +0300

    Create public components index file

---
+++ src/components/public/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Public components export
+ */
+
+export { default as LeagueHeader } from './LeagueHeader';
+export { default as RankingsTable } from './RankingsTable';
+export { default as MatchResults } from './MatchResults';
+export { default as UpcomingMatches } from './UpcomingMatches';
