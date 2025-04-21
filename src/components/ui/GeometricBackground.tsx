cb8f62b54d19577c7926fd5321265f8cd4a805b8
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-04-22 00:49:07 +0300

    Enhanced geometric background with improved shapes and animations

--- src/components/ui/GeometricBackground.tsx
+++ src/components/ui/GeometricBackground.tsx
@@ -18,18 +18,21 @@ export function GeometricBackground({
       shape2: 'opacity-15',
       shape3: 'opacity-10',
       shape4: 'opacity-5',
+      shape5: 'opacity-8',
     },
     subtle: {
       shape1: 'opacity-5',
       shape2: 'opacity-5',
       shape3: 'opacity-5',
       shape4: 'opacity-3',
+      shape5: 'opacity-4',
     },
     intense: {
       shape1: 'opacity-20',
       shape2: 'opacity-25',
       shape3: 'opacity-20',
       shape4: 'opacity-15',
+      shape5: 'opacity-18',
     },
   };
 
@@ -38,14 +41,15 @@ export function GeometricBackground({
     shape2: animated ? 'animate-float-delay-1' : '',
     shape3: animated ? 'animate-float-delay-2' : '',
     shape4: animated ? 'animate-float-delay-3' : '',
+    shape5: animated ? 'animate-float-delay-4' : '',
   };
 
   return (
     <div className={cn('absolute inset-0 overflow-hidden -z-10', className)}>
-      {/* Orange Triangle */}
+      {/* Orange Shape */}
       <div
         className={cn(
-          'absolute -top-20 -left-20 w-60 h-60 bg-padeliga-orange transform rotate-15',
+          'absolute -top-20 -left-20 w-64 h-64 bg-padeliga-orange transform rotate-15',
           opacityMap[variant].shape1,
           animationMap.shape1
         )}
@@ -54,7 +58,7 @@ export function GeometricBackground({
       {/* Teal Rectangle */}
       <div
         className={cn(
-          'absolute top-1/4 right-10 w-40 h-40 bg-padeliga-teal transform -rotate-12',
+          'absolute top-1/4 right-10 w-48 h-48 bg-padeliga-teal transform -rotate-12',
           opacityMap[variant].shape2,
           animationMap.shape2
         )}
@@ -63,7 +67,7 @@ export function GeometricBackground({
       {/* Purple Parallelogram */}
       <div
         className={cn(
-          'absolute bottom-20 left-1/4 w-60 h-40 bg-padeliga-purple transform rotate-45',
+          'absolute bottom-20 left-1/4 w-64 h-48 bg-padeliga-purple transform rotate-45',
           opacityMap[variant].shape3,
           animationMap.shape3
         )}
@@ -72,7 +76,7 @@ export function GeometricBackground({
       {/* Green Shape */}
       <div
         className={cn(
-          'absolute top-1/3 left-1/3 w-72 h-56 bg-padeliga-green transform skew-x-12 rotate-12',
+          'absolute top-1/3 left-1/3 w-80 h-64 bg-padeliga-green transform skew-x-12 rotate-12',
           opacityMap[variant].shape4,
           animationMap.shape4
         )}
@@ -81,9 +85,27 @@ export function GeometricBackground({
       {/* Red Shape */}
       <div
         className={cn(
-          'absolute -bottom-10 -right-10 w-40 h-40 bg-padeliga-red transform -rotate-15',
+          'absolute -bottom-10 -right-10 w-48 h-48 bg-padeliga-red transform -rotate-15',
           opacityMap[variant].shape1,
-          animationMap.shape4
+          animationMap.shape5
+        )}
+      />
+      
+      {/* Additional Teal Shape */}
+      <div
+        className={cn(
+          'absolute top-2/3 right-1/4 w-32 h-32 bg-padeliga-teal transform rotate-30',
+          opacityMap[variant].shape5,
+          animationMap.shape2
+        )}
+      />
+      
+      {/* Additional Orange Shape */}
+      <div
+        className={cn(
+          'absolute top-1/2 -left-10 w-40 h-40 bg-padeliga-orange transform -rotate-10',
+          opacityMap[variant].shape5,
+          animationMap.shape3
         )}
       />
     </div>
